f6bc9f3dbec03a0f5d8bdb24149950438d63d999
billly117 <bilalmesbahi@free.fr>
2026-05-25 16:29:05 +0200

    Add Javadoc documentation and MVP validation tests V2

--- src/module-info.java
+++ src/module-info.java
@@ -2,4 +2,10 @@
  * Main module of the graph-based agent simulation project.
  */
 module ProjetGenieLogiciel {
+
+    exports fr.projet;
+    exports fr.projet.model;
+    exports fr.projet.pathfinding;
+    exports fr.projet.simulation;
+    exports fr.projet.ui;
 }
