76d62274817da78b3a8ec3353f6cce5988630fbd
nassermohdelmi-sketch <nassermohdelmi01@gmail.com>
2026-05-27 18:39:04 +0200

    fix: add javafx modules to module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,12 @@
 /**
- * 
- */
-/**
- * 
+ * Main module of the graph-based agent simulation project.
  */
 module ProjetGenieLogiciel {
+    requires javafx.controls;
+    requires javafx.graphics;
+    exports fr.projet;
+    exports fr.projet.model;
+    exports fr.projet.pathfinding;
+    exports fr.projet.simulation;
+    exports fr.projet.ui;
 }
