88ac23a3206d9b71b6bd38fa6eb79e7fb374dd7a
Aminsfx <mohamedaminesbaiby@gmail.com>
2026-06-09 09:58:45 +0200

    style pause button yellow

--- src/fr/projet/ui/SimulationBar.java
+++ src/fr/projet/ui/SimulationBar.java
@@ -25,7 +25,8 @@ public class SimulationBar extends HBox {
         setStyle("-fx-background-color: #17202A;");
         setPadding(new Insets(8, 16, 8, 16));
         setAlignment(Pos.CENTER_LEFT);
-        style(startBtn, "> Start", "#1E8449", "#196F3D");
+        style(startBtn,  "> Start",  "#1E8449", "#196F3D");
+        style(pauseBtn,  "|| Pause", "#B7950B", "#9A7D0A");
         getChildren().addAll(
                 nextTickBtn,
                 startBtn,
